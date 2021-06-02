d26811ca2e77d162881839d1d1cba72e7a1105ad
Prem Ukkoji <premukkoji@Prems-MacBook-Air.local>
2021-06-02 12:22:32 +0530

    updated distance function and removed one bug

--- index.js
+++ index.js
@@ -12,10 +12,31 @@ class Point{
 };
 
 function distance(a, b){
-    var d1 = a.x - b.x;
-    var d2 = a.y - b.y;
+  lat1 = (a.x * 2.0 * Math.PI) / 60.0 / 360.0;      
+  long1 = (a.y * 2.0 * Math.PI) / 60.0 / 360.0;    
+  lat2 = (b.x * 2.0 * Math.PI) / 60.0 / 360.0;   
+  long2 = (b.y * 2.0 * Math.PI) / 60.0 / 360.0;       
 
-    return Math.sqrt( d1*d1 + d2*d2 );
+
+  // use to different earth axis length    
+  var a = 6378137.0;        // Earth Major Axis (WGS84)    
+  var b = 6356752.3142;     // Minor Axis    
+  var f = (a-b) / a;        // "Flattening"    
+  var e = 2.0*f - f*f;      // "Eccentricity"      
+
+  var beta = (a / Math.sqrt( 1.0 - e * Math.sin( lat1 ) * Math.sin( lat1 )));    
+  var cos = Math.cos( lat1 );    
+  var x = beta * cos * Math.cos( long1 );    
+  var y = beta * cos * Math.sin( long1 );    
+  var z = beta * ( 1 - e ) * Math.sin( lat1 );      
+
+  beta = ( a / Math.sqrt( 1.0 -  e * Math.sin( lat2 ) * Math.sin( lat2 )));    
+  cos = Math.cos( lat2 );   
+  x -= (beta * cos * Math.cos( long2 ));    
+  y -= (beta * cos * Math.sin( long2 ));    
+  z -= (beta * (1 - e) * Math.sin( lat2 ));       
+
+  return (Math.sqrt( (x*x) + (y*y) + (z*z) )/1000); 
 }
 
 function createEdge(a, b){
@@ -65,8 +86,6 @@ function dijkstras(start, end){
         Object.keys(edges[current_node]).forEach(element => {
             
             if(dist[element] > dist[current_node] + edges[current_node][element]){
-                edges[current_node][element] = dist[current_node] + edges[current_node][element];
-                edges[element][current_node] = dist[current_node] + edges[element][current_node];
                 parent[element] = current_node;
                 dist[element] = dist[current_node] + edges[current_node][element];
             }
